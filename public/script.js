3a024221a9ee65e50fadf799af6f24468f808744
Ponniah K <ponniahk78@gmail.com>
2020-12-28 02:05:49 +0530

    enable video controls

--- public/script.js
+++ public/script.js
@@ -4,21 +4,25 @@ const myPeer = new Peer(undefined, {
   host: '/',
   port: '3001'
 })
-const myVideo = document.createElement('video');
-myVideo.controls= true;
+// const myVideo = document.createElement('video');
+// myVideo.controls= true;
+const ownvideoGrid = document.getElementById('own-video-grid');
+const myOwnVideo = document.createElement('video');
+myOwnVideo.controls= true;
 //myVideo.muted = true
 const peers = {}
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
-  addVideoStream(myVideo, stream)
-
+  //addVideoStream(myVideo, stream);
+  addOwnVideoStream(myOwnVideo, stream)
   myPeer.on('call', call => {
     call.answer(stream)
     const video = document.createElement('video')
     call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream)
+     // addVideoStream(video, userVideoStream)
+     addOwnVideoStream(myOwnVideo, stream);
     })
   })
 
@@ -49,6 +53,14 @@ function connectToNewUser(userId, stream) {
 }
 
 function addVideoStream(video, stream) {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video)
+}
+
+function addOwnVideoStream(video, stream) {
   video.srcObject = stream
   video.addEventListener('loadedmetadata', () => {
     video.play()
