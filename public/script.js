2cffb9a962fe1d9bb846e9fd044e887b4daeaafe
Ponniah K <ponniahk78@gmail.com>
2020-12-28 16:13:21 +0530

    enable video controls

--- public/script.js
+++ public/script.js
@@ -1,10 +1,9 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-  host: 'bell-3streaming.herokuapp.com',
+var myPeer = new Peer('test', {
   secure:true,
+  host: 'bell-3streaming.herokuapp.com',
   port:443,
-  path:'/peerjs'
 })
 // const myVideo = document.createElement('video');
 // myVideo.controls= true;
