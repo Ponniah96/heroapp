03e6ec95911520a6362c813d77176f07f73071ee
Ponniah K <ponniahk78@gmail.com>
2020-12-29 18:38:19 +0530

    test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 const PORT=process.env.PORT || 3000;
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(__dirname + 'public'))
 
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
