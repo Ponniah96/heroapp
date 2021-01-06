12e165defe4a3f471f7b57145b9e9b5bba9032c6
Ponniah K <ponniahk78@gmail.com>
2021-01-06 18:23:24 +0530

    screen share

--- public/script.js
+++ public/script.js
@@ -17,29 +17,51 @@ myOwnVideo.controls= true;
 const peers = {};
 myOwnVideo.muted=true;
 myOwnVideo.poster="https://image.shutterstock.com/image-vector/vector-live-stream-icon-flat-260nw-1282569241.jpg"
-navigator.mediaDevices.getUserMedia({
-  video: true,
-  audio: true
-}).then(stream => {
-  console.log("Stream ",stream);
-  addOwnVideoStream(myOwnVideo, stream);
-  myPeer.on('call', call => {
-    call.answer(stream);
-    const video = document.createElement('video')
-    call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream);
-    })
-  })
+const startVideo=document.getElementById('camera-on');
+const stopVideo=document.getElementById('camera-off');
+startVideo.addEventListener("click", function (evt) {
+  startCamera();
+}, false);
 
-  socket.on('user-connected', userId => {
-    connectToNewUser(userId, stream)
-  })
-})
+stopVideo.addEventListener("click", function (evt) {
+  stopCamera();
+}, false);
+
+async function startCamera() {
+  try {
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    }).then(stream => {
+      console.log("Stream ",stream);
+      addOwnVideoStream(myOwnVideo, stream);
+        myPeer.on('call', call => {
+          call.answer(stream);
+          const video = document.createElement('video')
+          call.on('stream', userVideoStream => {
+          addVideoStream(video, userVideoStream);
+        })
+      })
+    
+      socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+      })
+    })
+  } catch (err) {
+    console.error("Error: " + err);
+  }
+}
+
+function stopCamera() {
+  let tracks = myOwnVideo.srcObject.getTracks();
+  tracks.forEach(track => track.stop());
+  myOwnVideo.srcObject = null;
+}
 
 socket.on('user-disconnected', userId => {
+  console.log("User Disconnected",userId);
   if (peers[userId]) peers[userId].close()
 })
-
 
 function connectToNewUser(userId, stream) {
   const call = myPeer.call(userId, stream)
@@ -65,7 +87,7 @@ function addVideoStream(video, stream) {
 function addOwnVideoStream(video, stream) {
   video.srcObject = stream
   video.addEventListener('loadedmetadata', () => {
-    video.pause()
+    video.play()
   })
   ownvideoGrid.append(video)
 }
