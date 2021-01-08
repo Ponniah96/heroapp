710ccd30969234cc277a3a654b7a588ff3cd9fb1
Ponniah K <ponniahk78@gmail.com>
2021-01-08 20:52:53 +0530

    screen share

--- public/script.js
+++ public/script.js
@@ -125,7 +125,7 @@ function addVideoStream(video, stream) {
 function addOwnVideoStream(video, stream) {
   video.srcObject = stream
   video.addEventListener('click', () => {
-    video.stop()
+    video.remove()
   })
   ownvideoGrid.append(video)
 }
