0765a8f61a63ba1588d1d5f715741f4ad8c27af8
Ponniah K <ponniahk78@gmail.com>
2021-01-11 19:23:37 +0530

    screen share

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
+const router =express.Router();
 const path = require('path');
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 const PORT=process.env.PORT || 3000;
+const home=require('./public/home');
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/home',home);
 
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
@@ -16,6 +20,10 @@ app.get('/', (req, res) => {
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
+
+// app.get('/home',(req, res)=>{
+//   res.render('home');
+// })
 
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
