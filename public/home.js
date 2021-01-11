9b4e3ff29971297ff2fc788aee963bb90cd6364d
Ponniah K <ponniahk78@gmail.com>
2021-01-11 19:37:19 +0530

    screen share

--- public/home.js
+++ public/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const router =express.Router();
 
-router.get('/home',(req, res)=>{
+router.get('/',(req, res)=>{
   res.render('home');
 });
 
