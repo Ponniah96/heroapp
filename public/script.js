42dc5f7e04c687fdce3ad3fee0d78d5b5be3a285
Ponniah K <ponniahk78@gmail.com>
2021-01-13 02:25:39 +0530

    screen share

--- public/script.js
+++ public/script.js
@@ -102,7 +102,7 @@ function addVideoStream(video, stream) {
     videoCapture.play();
     parentVideo.append(videoCapture);
     localStorage.setItem('video',parentVideo.outerHTML);
-    console.log("Streaming Videos",parentVideo);
+    console.log("Streaming Videos srcobject",videoCapture.srcObject);
   });
   videoGrid.append(video);
 }
