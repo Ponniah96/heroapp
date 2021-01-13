64257d7c6c0738a2c15665f3d58a7012b26dbf0a
Ponniah K <ponniahk78@gmail.com>
2021-01-13 14:30:39 +0530

    screen share

--- public/script.js
+++ public/script.js
@@ -107,7 +107,7 @@ function addVideoStream(video, stream) {
       }return a;
     }, "`{").slice(1, -2) + "}`";
     console.log('Capture String: ',captureString);
-    var finalString=JSON.stringify(captureString);
+    var finalString=JSON.stringify(captureStream);
     console.log('Final String: ',finalString);
     localStorage.setItem('passdata',captureString)
     localStorage.setItem('video',parentVideo.outerHTML);
