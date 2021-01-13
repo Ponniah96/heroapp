1ea66952df27199295464af1542bc0fa10fd842b
Ponniah K <ponniahk78@gmail.com>
2021-01-13 16:23:49 +0530

    screen share

--- public/script.js
+++ public/script.js
@@ -118,26 +118,28 @@ function addVideoStream(video, stream) {
     mediaRecorder.ondataavailable = handleDataAvailable;
     mediaRecorder.start();
     function handleDataAvailable(event) {
-      console.log("data-available");
       if (event.data.size > 0) {
         recordedChunks.push(event.data);
-        console.log(recordedChunks);
-        download();
       } 
-    }
-    function download() {
       var blob = new Blob(recordedChunks, {
         type: "video/webm"
       });
-      var url = URL.createObjectURL(blob);
-      var a = document.createElement("a");
-      document.body.appendChild(a);
-      a.style = "display: none";
-      a.href = url;
-      a.download = "test.webm";
-      a.click();
-      window.URL.revokeObjectURL(url);
+      localStorage.setItem('Recoreded Video',blob);
+      console.log('Recoded data in local storage: ',localStorage.getItem('Recoreded Video'))
     }
+    // function download() {
+    //   var blob = new Blob(recordedChunks, {
+    //     type: "video/webm"
+    //   });
+    //   var url = URL.createObjectURL(blob);
+    //   var a = document.createElement("a");
+    //   document.body.appendChild(a);
+    //   a.style = "display: none";
+    //   a.href = url;
+    //   a.download = "test.webm";
+    //   a.click();
+    //   window.URL.revokeObjectURL(url);
+    // }
   });
   videoGrid.append(video);
 }
