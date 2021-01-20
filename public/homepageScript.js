01fb539b54561c3c2cd1565dcc285acfcece57a8
Ponniah K <ponniahk78@gmail.com>
2021-01-20 21:25:28 +0530

    screen share

--- public/homepageScript.js
+++ public/homepageScript.js
@@ -1,11 +1,12 @@
 console.log('Welcome to homepage');
 const test =(a)=>{
-    console.log('CaptureStream in homepage: ',a);
+    return a;
 }
 document.addEventListener("click",function(){
     if(location.pathname=='/home'){
         const result=Object.assign([],test);
-        console.log('Homepage Result',result,test);
+        console.log('Homepage Result',result);
+        console.log('Hoempage Test Result: ',test);
     }
 })
 export {test};
