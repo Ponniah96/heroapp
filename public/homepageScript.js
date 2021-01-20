5d5487ca6522156e25e75a1689dbb31bf66ab849
Ponniah K <ponniahk78@gmail.com>
2021-01-20 17:36:16 +0530

    screen share

--- public/homepageScript.js
+++ public/homepageScript.js
@@ -18,6 +18,13 @@
             // }
             const test =(a)=>{
                 console.log('Capture Stream: ',a);
+                if(window.location.pathname=='/home'){
+                const parentVideos = document.getElementById('captureStreams');
+                const video = document.createElement('video');
+                video.srcObject = null;
+                video.play();
+                parentVideos.append(video);
+                }
             }
             export {test};
             
