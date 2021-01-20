1969eda523ba30ca5a9d4a350f2e0bfb442c3111
Ponniah K <ponniahk78@gmail.com>
2021-01-20 22:36:58 +0530

    screen share

--- public/homepageScript.js
+++ public/homepageScript.js
@@ -1,22 +1,8 @@
-//import {strearesult} from './script.js'
+import {strearesult} from './script.js'
 
 console.log('Welcome to homepage');
 
-const test =(a)=>{
-  return a;
-}
-document.addEventListener("click",function(){
-  if(location.pathname=='/home'){
-      const result=test;
-      console.log('Homepage Result',result);
-      const parentVideos = document.getElementById('captureStreams');
-      const video = document.createElement('video');
-      video.srcObject = result;
-      video.play();
-      parentVideos.append(video);
-  }
-})
-export {test};
+console.log("medisatearmresult: ",strearesult);
 
            
             
