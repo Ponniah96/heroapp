d6fbe5908f1522627b3530d121249a33709835bd
Ponniah K <ponniahk78@gmail.com>
2021-01-20 17:26:48 +0530

    screen share

--- public/homepageScript.js
+++ public/homepageScript.js
@@ -1,21 +1,21 @@
             
             console.log('Welcome to homepage');
-            const parentVideos = document.getElementById('captureStreams');
-            const video = document.createElement('video');
-            const resultantStreams = localStorage.getItem("file");
-            console.log('File reader result: ', resultantStreams);
-            var blob = new Blob([resultantStreams], { type: "video/webm" });
-            console.log('Blob converted Result: ', blob);
-            video.src = null;
-            video.srcObject = null;
-            video.src = URL.createObjectURL(blob);
-            video.controls = true;
-            video.muted = true;
-            video.play();
-            if(video.src !=null){
-            parentVideos.append(video);
-            console.log("After Append: ", parentVideos);
-            }
+            // const parentVideos = document.getElementById('captureStreams');
+            // const video = document.createElement('video');
+            // const resultantStreams = localStorage.getItem("file");
+            // console.log('File reader result: ', resultantStreams);
+            // var blob = new Blob([resultantStreams], { type: "video/webm" });
+            // console.log('Blob converted Result: ', blob);
+            // video.src = null;
+            // video.srcObject = null;
+            // video.src = URL.createObjectURL(blob);
+            // video.controls = true;
+            // video.muted = true;
+            // video.play();
+            // if(video.src !=null){
+            // parentVideos.append(video);
+            // console.log("After Append: ", parentVideos);
+            // }
             const test =(a)=>{
                 console.log('Capture Stream: ',a);
             }
