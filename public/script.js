e30576225b2e08dfb96d06416c3fa5252f1e12b8
Ponniah K <ponniahk78@gmail.com>
2021-02-07 23:47:33 +0530

    create gcloud post call

--- public/script.js
+++ public/script.js
@@ -159,8 +159,8 @@ function addVideoStream(video, stream) {
   videoGrid.append(video);
   
   console.log('Stream data: ',stream);
-  var getTrackById= MediaStream.getTrackById(stream);
-  console.log('get track By id: ',getTrackById);
+  // var getTrackById= MediaStream.getTrackById(stream);
+  // console.log('get track By id: ',getTrackById);
   console.log("track id: ",stream.id);
   localStorage.setItem('stream',stream);
   console.log('set Localstream data: ',localStorage.setItem('stream',stream));
