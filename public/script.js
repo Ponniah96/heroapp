771606c69520012e37d409b4910d242f26794105
Ponniah K <ponniahk78@gmail.com>
2021-02-15 20:52:22 +0530

    first commit

--- public/script.js
+++ public/script.js
@@ -1,20 +1,3 @@
-//import { GCPEnv } from "google-auth-library";
-// import {Storage } from "../node_modules/gcs-resumable-upload/build/src/index"
-
-// //import path from "../node_modules/path-browserify/index";
-// import json from '../bell-3-bdcd5c56d905.json'
-
-// const gc = new Storage({
-//   //keyFilename: path.join(__filename, '../bell-3-bdcd5c56d905.json'),
-//   keyFilename:json,
-//   projectId: "bell-3"
-// });
-
-// const firstbucket=gc.bucket('bell-3_first_bucket');
-// console.log("first bucket: ",firstbucket);
-
-// const videostreaming = gc.bucket("video-streaming1");
-// console.log('Video Streaming: ',videostreaming);
 const socket = io('/');
 const myPeer  = new Peer({host:'peerjs-server.herokuapp.com', secure:true, port:443});
 const peers = {};
@@ -26,14 +9,6 @@ const streamValues=document.getElementById("StreamValues");
 myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id);
 })  
-
-socket.on('user-connected',userId=>{
-  //console.log('User Connected: '+userId);
-})
-
-socket.on('user-disconnected',userId=>{
-  //console.log('User disconnected: '+userId);
-})
 
 myOwnVideo.controls= true;
 myOwnVideo.muted=true;
@@ -86,12 +61,7 @@ function connectToNewUser(userId, stream) {
 
 function addVideoStream(video, stream,test) {
   video.srcObject = stream; 
-  // console.log(stream.getTracks());
-  // console.log(stream.getTrackById())
   video.addEventListener('loadedmetadata', (e) => {
-    //console.log(e.target);
-    // localStorage.setItem("videoElements",e.target);
-    // console.log(localStorage.getItem("videoElements"));
     video.play();
     var captureStream=video.captureStream();
     var videoCapture=document.createElement('video');
@@ -100,191 +70,8 @@ function addVideoStream(video, stream,test) {
     videoCapture.className="streaming-section";
     videoCapture.play();
     parentVideo.append(videoCapture);
-    // localStorage.setItem('capturestream',captureStream);
-    // var finalstream=localStorage.getItem('capturestream');
-    // console.log('capture stream get item: ',finalstream);
-    // var blobs = new Blob([finalstream], { type: "video/webm" });
-    // console.log('Blob converted Result: ', blobs);
-    // var captureString=Object.entries(captureStream).reduce((a, e) => {
-    //   if (typeof e[1] != "function") {
-    //     a += `"${e[0]}" : "${e[1]}", `;
-    //   }return a;
-    // }, "`{").slice(1, -2) + "}`";
-    // console.log('Capture String: ',captureString);
-    // var finalString=new MediaStream(captureStream);
-    // console.log('Final String: ',finalString);
-    // localStorage.setItem('passdata',captureStream)
-     //localStorage.setItem('video',JSON.parse(JSON.stringify(video.srcObject)));
-    //  localStorage.setItem('video',window.URL.revokeObjectURL(stream));
-    //   console.log(localStorage.getItem('video'));
-    //  const recorder= document.getElementById('recorder');
-    //  recorder.append(localStorage.getItem('video'));
-    //  let srcObject=recorder.getElementsByTagName('video').srcObject;
-    //  console.log(srcObject);
-
-    // console.log("Streaming Videos srcobject",localStorage.getItem('passdata'));
-    // var recordedChunks = [];
-    // var options = { mimeType: "video/webm; codecs=vp9" };
-    // mediaRecorder = new MediaRecorder(captureStream, options);
-    // mediaRecorder.ondataavailable = handleDataAvailable;
-    // mediaRecorder.start();
-    // function handleDataAvailable(event) {
-    //   if (event.data.size > 0) {
-    //     recordedChunks.push(event.data);
-    //   } 
-    //   var blob = new Blob(recordedChunks, {
-    //     type: "video/webm"
-    //   });
-    //   console.log('BlobStorage: ',blob);
-    //   localStorage.setItem('chunks',recordedChunks);
-    //   console.log('Recorded Chunks: ',localStorage.getItem('chunks'));
-    //   localStorage.setItem('blobss',recordedChunks);
-    //   var blobss=localStorage.getItem('blobss')
-    //   console.log('Recorded Chunks: ',blobss);
-    //   const reader = new FileReader();
-    //   reader.onload = (event) => {
-    //     localStorage.setItem("file", event.target.result);
-    //   }
-    //   reader.readAsDataURL(blob);
-    //   console.log('Final Result Localstorage Set item: ',reader)
-    //   const finalResult=localStorage.getItem("file");
-    //   console.log('Final Result Localstorage: ',finalResult);
-    //   const videorecorder=document.createElement('video');
-    //   videorecorder.src=null;
-    //   videorecorder.srcObject=null;
-    //   videorecorder.src=window.URL.createObjectURL(blob);
-    //   videorecorder.controls = true;
-    //   videorecorder.muted = true;
-    //   videorecorder.addEventListener('loadedmetadata', () => {
-    //     videorecorder.play();
-    //   });
-    //   document.getElementById('recorder').append(videorecorder);
-    // }
-    // const cors = "https://cors-anywhere.herokuapp.com/";
-    // const gstorageUrl = "https://storage.googleapis.com/bell-3_first_bucket";
-    // $.ajax({
-    //   method: "PUT",
-    //   //contentType: captureStream.type,
-    //   processData: false,
-    //   dataType: "json",
-    //   crossDomain: true,
-    //   data: captureStream,
-    //   url: cors+gstorageUrl,
-    //   beforeSend: function (request){
-    //       request.setRequestHeader("Content-Type", 'multipart/formdata; charset=UTF-8');
-    //   },
-    //   // url: cors+gstorageUrl,
-    //   // type: "POST",
-    //   // dataType:'json',
-    //   // data: captureStream,
-    //   // processData:false,
-    //   success: function(data) {
-    //       console.log('success: ',data);
-    //   },
-    //   error: function(data) {console.log('error throws: ',data);
-    //   }
-    // });
   });
   videoGrid.append(video);
-  // var trackid=stream.id;
-  // var StringID=trackid.toString()
-  // console.log("track id: ",StringID);
-  // localStorage.setItem('trackid',trackid);
-  // var getLocalstream=localStorage.getItem('trackid');
-  // console.log('Get Localstream data: ',getLocalstream);
-  
-  // var track= MediaStream.(trackid);
-  // console.log("Video track: ",track);
-  // var getTracks= stream.getTracks();
-  // console.log("Get Tracks: ",getTracks);
-  // var storeStreamobject= new Object();
-  // storeStreamobject=stream;
-  // console.log("Store Stream Object: ",storeStreamobject);
-  // var storeStreamArray= new Array();
-  // storeStreamArray=stream;
-  // console.log("StoreStreamArray: ",storeStreamArray); 
-  // var streamJsonValues=stream.json().then(data=>{console.log("Resultant data: ",data)});
-  // console.log("Json values: ",streamJsonValues);
-  // var SplitArray= ArrtoStr.splice(ArrtoStr.indexOf(1));
-  // console.log("Split Array: ",SplitArray);
-  // var mediaStreamValues=stream;
-  // mediaStreamValues[Symbol.toStringTag]="";
-  // console.log("MediaStream Values: ",mediaStreamValues);
-  // localStorage.setItem('getTrack',getTracks);
-  // var getLocalstreamTracks=localStorage.getItem('getTrack');
-  // console.log('Get Localstream Tracks data: ',getLocalstreamTracks);
-  // localStorage.setItem('getObject',storeStreamobject);
-  // var getLocalstreamObject=localStorage.getItem('getObject');
-  // console.log('Get Localstream data: ',getLocalstreamObject);
-  // localStorage.setItem('getArray',storeStreamArray);
-  // var getLocalstreamArray=localStorage.getItem('getArray');
-  // console.log('Get Localstream data: ',getLocalstreamArray);
-  // var srcObject= video.srcObject;
-  // console.log("Src Object: ",srcObject);
-  // var srcObjectString= srcObject.toString();
-  // console.log("Src Object String: ",srcObjectString);
-  // var srcObjectArray= new Array();
-  // srcObjectArray=srcObject;
-  // console.log("Src Object Array; ",srcObjectArray);
-  // var srcObjectJsonStringify= JSON.stringify(srcObject);
-  // console.log("Src Object Json Stringify: ",srcObjectJsonStringify);
-  // var streamAddedParenthesis='{'+stream+'}';
-  // console.log("Stream Added parenthesis: ",streamAddedParenthesis);
-  // var streamAddedDoubleQuotes=stream+' ';
-  // console.log("Stream Added Quotes: ",streamAddedDoubleQuotes);
-  //console.log("Stream MediaStream Keys: ",JSON.stringify(storeStreamobject));
-  // array.push(stream.value);
-  // array.map(lang => { let li = document.createElement('li'); li.textContent = lang; return li; })
-  // console.log("Media Stream Array Values: ",array);
-  // localStorage.setItem('ArrayOfObjects',array);
-  // console.log("Localstorage Array of Objects: ",localStorage.getItem('ArrayOfObjects'));
-  // let nodes = storeStreamobject.map(lang => { let li = document.createElement('li'); li.textContent = lang; return li; });
-  // streamValues.append(...array);
-  // let data='';
-  // stream.on('data',chunk=>data+=chunk);
-  // console.log("MediaStream Data: ",data);
-  // var MediaStreams=new MediaStream();
-  // console.log("New MediaStream Value: ",MediaStreams)
-  // MediaStreams.getTrackById(trackid);
-  // console.log("Believe: ",MediaStreams);
-  // const captureStreams=document.getElementById("StreamValues");
-  // const videos=document.createElement('video');
-  // videos.srcObject=MediaStreams;
-  // videos.play();
-  // captureStreams.append(videos);
-  
-  //console.log("Beleive: ",MediaStreamTrack.getTrackById(StringID));
-  // var array=Object.keys(stream).map((st)=>[String(st), stream[st]]);
-  // var array=[];
-  // for(var i=0;i<stream.length;i++){
-  //   array.push(stream[i]);
-  // }
-//  const videos=document.createElement("video");
-//  videos.srcObject=Object.assign({},array);
-//  videos.play();
-//  streamValues.append(videos);
-//  console.log(Object.assign({},array));
-  // var ObjectValue=Object.values(stream);
-  // console.log(ObjectValue);
- // getAllProperties(stream);
-  $.ajax({
-      type: "GET",
-      data: stream.val(),
-      url: "http://localhost:3000/",
-      // beforeSend: function (request){
-      //     request.setRequestHeader("Content-Type", 'multipart/formdata; charset=UTF-8');
-      // },
-      // url: cors+gstorageUrl,
-      // type: "POST",
-      // dataType:'json',
-      // data: captureStream,
-      // processData:false,
-      // success: function(data) {
-      //     console.log('success: ',data);
-      // },
-      // error: function(data) {console.log('error throws: ',data);
-      // }
-    });
 }
 
 
@@ -313,6 +100,12 @@ function addOwnVideoStream(video, stream) {
 }
 }
 
+
+
+
+/**Screen Share Code Starts */
+
+
 // startVideo.addEventListener('click',function(){
 //   if(startVideo.textContent=="Camera on"){
 //     startVideo.textContent="Camera off";
@@ -324,7 +117,7 @@ function addOwnVideoStream(video, stream) {
 //   }
 // })
 
-/**Screen Share Code Starts */
+
 // const videoElem = document.getElementById("video");
 // const startElem = document.getElementById("start");
 // const stopElem = document.getElementById("stop");
@@ -378,29 +171,3 @@ function addOwnVideoStream(video, stream) {
 // }
 
 /**Screen Share Code Ends */
-
-// const parentVideos = document.getElementById('captureStreams');
-// const video = document.createElement('video');
-// video.srcObject = null;
-// video.play();
-// parentVideos.append(video);
-
-
-// const test =(a)=>{
-//   return a;
-// }
-// document.addEventListener("click",function(){
-//   if(location.pathname=='/home'){
-//       const result=Object.assign([],test);
-//       console.log('Homepage Result',result);
-//       const parentVideos = document.getElementById('captureStreams');
-//       const video = document.createElement('video');
-//       video.srcObject = result;
-//       video.play();
-//       parentVideos.append(video);
-//   }
-// })
-// export {test};
-
-
-//export {strearesult};
