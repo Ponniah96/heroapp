4f4d25d814de8c9c2ba3facb74b316bfcc5d2dba
Ponniah96 <75694986+Ponniah96@users.noreply.github.com>
2021-02-20 19:02:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,48 +1,37 @@
 const express = require('express');
 const app = express();
+const router =express.Router();
 const path = require('path');
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 const PORT=process.env.PORT || 3000;
 const home=require('./public/home');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+extended: true
+}));
+app.use(bodyParser.json());
+
+app.set('views',path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/home',home);
-
-/**Integrate Google cloud Storage into our application Starts */
-// Imports the Google Cloud client library
-const {Storage} = require('@google-cloud/storage');
-
-const gc = new Storage({
-  keyFilename: path.join(__filename, '../bell-3-bdcd5c56d905.json'),
-  projectId: "bell-3"
-});
-
-const firstbucket=gc.bucket('bell-3_first_bucket');
-console.log("first bucket: ",firstbucket);
-
-const videostreaming = gc.bucket("video-streaming1");
-console.log('Video Streaming: ',videostreaming);
-/**Integrate Google cloud Storage into our application Ends */
-
 app.get('/', (req, res) => {
-  console.log("stream value:",res)
-  res.redirect(`/${uuidV4()}`)
+  res.redirect(`/${uuidV4()}`);
 })
 
 app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
+  res.render('index', { roomID: req.params.room });
 })
-
 
 /**Integrate Socket into our application Starts */
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
-  console.log("socket connected: ",roomId,userId);
+    console.log("socket connected: ",roomId,userId);
     socket.join(roomId);
     socket.to(roomId).broadcast.emit('user-connected', userId);
     
@@ -54,9 +43,17 @@ io.on('connection', socket => {
 
 /**Integrate Socket into our application Ends */
 
+app.get('/streaming/:room',(req, res)=>{
+  res.render('home', { roomID: req.params.room });
+  console.log("RenderHomePageHTML");
+});
+
+app.get('/video-call/:room',(req, res)=>{
+  res.render('room', { roomID: req.params.room });
+  console.log("RenderVideoCallPageHTML");
+});
+
 server.listen(PORT);
-
-
 
 
 
