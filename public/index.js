23526dd48a821e8f49b99200e5a3cf6af03333e6
Ponniah96 <75694986+Ponniah96@users.noreply.github.com>
2021-02-20 19:08:34 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -3,17 +3,6 @@
     var stream=document.getElementById('stream');
     stream.addEventListener('click',function(){
         location.href='/streaming/'+ ROOM_ID;
-        // var url = '/streaming'
-        // $.ajax({
-        //     url: url,
-        //     type: 'GET',
-        //     success: function (data) {
-        //         console.log('success',data);
-        //     },
-        //     error: function () {
-        //         console.log('error');
-        //     }
-        // })
     })
     var call=document.getElementById('call');
     call.addEventListener('click',function(){
